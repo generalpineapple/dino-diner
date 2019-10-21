efe06b0b623e6e660086a8cee2b151a6fa0f1e4b
Colden Miller <generalpineapple@ksu.edu>
2019-10-20 22:51:48 -0500

    created and tested order

---
+++ Menu/Order.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu
+{
+    public class Order
+    {
+        /// <summary>
+        /// Represents the items added to the order
+        /// </summary>
+        public ObservableCollection<IOrderItem> Items { get; set; }
+
+        public Order()
+        {
+            Items = new ObservableCollection<IOrderItem>();
+        }
+
+        /// <summary>
+        /// This calculates the total price from the prices of all order items
+        /// </summary>
+        public double SubtotalCost {
+            get
+            {
+                double total = 0;
+                foreach(IOrderItem item in Items)
+                {
+                    total += item.Price;
+                }
+                if (total < 0) return 0;
+                return total;
+            }
+        }
+
+        /// <summary>
+        /// Gets the Sales rate tax and is able to be set
+        /// </summary>
+        public double SalesTaxRate { get; protected set; } = .1;
+
+        /// <summary>
+        /// This is the cost of the sales tax
+        /// which is the product of the SalesTaxRate and the SubtotalCost
+        /// </summary>
+        public double SalesTaxCost {
+            get
+            {
+                return SalesTaxRate * SubtotalCost;
+            }
+        }
+
+        /// <summary>
+        /// This is the total cost of everything
+        /// which is the sum of the SubtotalCost and the SalesTaxCost.
+        /// </summary>
+        public double TotalCost {
+            get
+            {
+                return SubtotalCost + SalesTaxCost;
+            }
+        }
+
+    }
+}
---
+++ MenuTest/OrderTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DinoDiner.Menu;
+
+namespace MenuTest
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void ShouldHaveExpectedSubTotal()
+        {
+            Order order = new Order();
+            order.Items.Add(new Brontowurst());
+            order.Items.Add(new Fryceritops());
+            order.Items.Add(new Sodasaurus());
+            Assert.Equal(7.85, order.SubtotalCost, 2);
+        }
+
+        [Fact]
+        public void ShouldHaveExpectedRateCost()
+        {
+            Order order = new Order();
+            order.Items.Add(new Brontowurst());
+            order.Items.Add(new Fryceritops());
+            order.Items.Add(new Sodasaurus());
+            Assert.Equal(.79, order.SalesTaxCost, 2);
+        }
+
+        [Fact]
+        public void ShouldHaveExpectedTotalCost()
+        {
+            Order order = new Order();
+            order.Items.Add(new Brontowurst());
+            order.Items.Add(new Fryceritops());
+            order.Items.Add(new Sodasaurus());
+            Assert.Equal(8.64, order.TotalCost, 2);
+        }
+
+        [Fact]
+        public void ShouldNeverBeNegative()
+        {
+            //ToDo
+        }
+    }
+}
